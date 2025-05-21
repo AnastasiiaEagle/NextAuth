5c7515ff1258603cb4121fadfe8d8cc816d1cea1
Dellari0n <anastasia.ea9le@gmail.com>
2025-05-21 11:57:21 +0300

    start

---
+++ client/src/app/page.tsx
@@ -0,0 +1,57 @@
+'use client'
+
+import Image from "next/image";
+import { jwtDecode } from "jwt-decode";
+import { useRouter } from "next/navigation";
+import { useEffect } from "react";
+import Cookies from 'js-cookie';
+import axios from '../utils/axios'
+
+
+export default function Home() {
+  const router = useRouter();
+
+  function isAccessTokenValid(token: string): boolean {
+        try {
+            const decoded: { exp: number } = jwtDecode(token);
+            const currentTime = Date.now() / 1000;
+            return decoded.exp > currentTime;
+        } catch (error) {
+            return false;
+        }
+    }
+
+    const tokenSearch = async()=>{
+        const localToken = localStorage.getItem('accessToken');
+        
+        if(!localToken){
+            console.log('Токен не знайдений');
+            router.push('/auth');
+        }else if(localToken !== null){
+          if(!isAccessTokenValid(localToken)){
+            try {
+                const res = await axios.get('/auth/refresh',
+                {
+                    withCredentials: true
+                }
+                );
+                if (res.data.accessToken) {
+                    localStorage.setItem('accessToken', res.data.accessToken);
+                }
+            } catch (error: any) {
+                console.log(error)
+            }
+        }
+      }
+    }
+
+  useEffect(() => {
+    tokenSearch()
+  }, []);
+
+  return (
+    <>
+      Привіт
+    </>
+  );
+}
